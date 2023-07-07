b903b9a33799444b8af67ef507069999d868db53
Shoxrux Sunnatov <orozboyjigit@gmail.com>
2023-07-07 17:40:26 +0500

    add sponsor model

---
+++ sponsor/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Sponsor(models.Model):
+
+    class StatusChoices(models.Choices):
+        NEW = 'new', _('New')
+        IN_PROCESS = 'in_process', _('In process')
+        CONFIRMED = 'confirmed', _('Confirmed')
+        CANCELLED = 'cancelled', _('Cancelled')
+
+    full_name = models.CharField(max_length=250)
+    phone = models.CharField(max_length=30)
+    amount = models.PositiveBigIntegerField()
+    is_organization = models.BooleanField()
+    status = models.CharField(max_length=30, choices=StatusChoices.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    organization_name = models.CharField(max_length=250)
