2f79bf367fe6f63356de86c14c434ba325fbbc87
davlat <davlatbekm004@gmail.com>
2023-07-10 18:30:00 +0500

    some changes

--- sponsor/views.py
+++ sponsor/views.py
@@ -1,10 +1,11 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
+from rest_framework.views import APIView
+
 from .serializers import SponsorSerializer
 
 
-class SponsorApi(APIView):
+class SponsorCreateAPIView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = SponsorSerializer(data=request.data)
 
