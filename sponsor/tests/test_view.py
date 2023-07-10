2332542649f9777bf6f6a94a45c68ba2e462cd6c
Odina <md.dilyaramustafaeva@gmail.com>
2023-07-10 18:52:38 +0500

    is_organization true , False

--- sponsor/tests/test_view.py
+++ sponsor/tests/test_view.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 class TestApplicationFormView:
-    def test_create_application_form(self, client):
+    def test_create_application_form_true(self, client):
         url = reverse("sponsor-create")
         data = {
             "id": 1,
@@ -12,8 +12,20 @@ class TestApplicationFormView:
             "phone": "+9987234354",
             "amount": 34,
             "is_organization": True,
-            "status": "in_process",
-            "created_at": "7.10.2023",
+            "organization_name": "Book",
+        }
+
+        response = client.post(url, data=data)
+        assert response.status_code == 201
+
+    def test_create_application_form_false(self, client):
+        url = reverse("sponsor-create")
+        data = {
+            "id": 1,
+            "full_name": "xx xx",
+            "phone": "+9987234354",
+            "amount": 34,
+            "is_organization": False,
             "organization_name": "Book",
         }
 
