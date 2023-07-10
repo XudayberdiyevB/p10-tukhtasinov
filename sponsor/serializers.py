bb5be61def4c1323e3600b81c521870770405027
davlat <davlatbekm004@gmail.com>
2023-07-10 18:51:08 +0500

    some changes

--- sponsor/serializers.py
+++ sponsor/serializers.py
@@ -6,4 +6,4 @@ from .models import Sponsor
 class SponsorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sponsor
-        fields = ("id", "full_name", "phone", "amount", "is_organization", "status", "created_at", "organization_name")
+        fields = ("id", "full_name", "phone", "amount", "is_organization", "organization_name")
