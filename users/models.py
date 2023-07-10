59d142f4730212861b28f9331949cc013200cf93
oktamov <122670933+oktamov@users.noreply.github.com>
2023-07-10 18:33:05 +0500

    user login pytest commit

--- users/models.py
+++ users/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255, null=True)
-    password = models.CharField()
 
     def __str__(self):
         return self.username
