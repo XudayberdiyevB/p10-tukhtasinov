50bf90be3c3f3cfe2bc01824942e997cbe3ecc35
BunyodNaimov <naimov.bunyod2021@gmail.com>
2023-07-12 17:52:52 +0500

    fix user model

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from users.views import UserLoginAPIView, UserRegisterView
+from users.views import UserLoginAPIView, UserRegisterAPIView
 
 
 urlpatterns = [
-    path("register/", UserRegisterView.as_view(), name="user-register"),
+    path("register/", UserRegisterAPIView.as_view(), name="user-register"),
     path("login/", UserLoginAPIView.as_view(), name="user-login"),
 ]
