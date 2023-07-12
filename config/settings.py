b1e91c0094ed6b4d7f01def0fb353ff0def2c914
Saidahror13 <01erkhinov@gmail.com>
2023-07-12 17:34:12 +0500

    ok)

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, "config/.env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -94,8 +94,8 @@ DATABASES = {
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
-        "HOST": 'localhost',
-        "PORT": 5432,
+        "HOST": env("DB_HOST"),
+        "PORT": env('DB_PORT'),
     }
 }
 
