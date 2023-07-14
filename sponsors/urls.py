76103722a188eebd772ff08102e700b090a88363
05Muhammadali <pubgm.muhammadali@gmail.com>
2023-07-14 16:35:08 +0500

    Sponsor List For Select Urls

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -4,6 +4,7 @@ from sponsors.views import (
     SponsorDetailView,
     SponsorListCreateView,
     SponsorMoneyDashboard,
+    SponsorListForSelect,
 )
 
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("", SponsorListCreateView.as_view(), name="sponsor_list_create"),
     path("<int:pk>/", SponsorDetailView.as_view(), name="sponsor_detail"),
     path("dashboard/", SponsorMoneyDashboard.as_view(), name="sponsors-dashboard"),
+    path("sponsor_select/", SponsorListForSelect.as_view(), name="sponsor-list-select"),
 ]
